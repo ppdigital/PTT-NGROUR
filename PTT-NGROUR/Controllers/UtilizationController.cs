56bbaafa0e83f691c07bb99a9907b6e992ef595a
porconan <porconan12@hotmail.com>
2018-06-04 19:28:39 +0700

    add SiteMap and Bootstrap

---
+++ PTT-NGROUR/Controllers/UtilizationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PTT_NGROUR.Controllers
+{
+    public class UtilizationController : Controller
+    {
+        //
+        // GET: /Utilization/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
