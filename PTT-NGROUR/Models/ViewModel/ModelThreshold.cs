11eeb9f68338aff4dee3f8bc93a8160c2bbb7963
FrankMusix <frankinpark2500@hotmail.com>
2018-06-05 09:25:42 +0700

    UPdate Threshold

---
+++ PTT-NGROUR/Models/ViewModel/ModelThreshold.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PTT_NGROUR.Models.ViewModel
+{
+    public class ModelThreshold
+    {
+        public ModelThresholdItem[] ThresholdItems { get; set; }
+    }
+
+    public enum EnumThresholdType
+    {
+        None , GateStation, PipeLine
+    }
+
+    public class ModelThresholdItem
+    {
+        public int ThresholdId { get; set; }
+        public string Color { get; set; }
+        public EnumThresholdType ThresholdType { get; set; }
+        public decimal MinValue { get; set; }
+        public decimal MaxValue { get; set; }
+    }
+}
