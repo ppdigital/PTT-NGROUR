46961ac5c67fda709735d58f48bc4d7ef4b0d1e7
porconan <porconan12@hotmail.com>
2018-06-05 15:37:08 +0700

    Add Admin Controller and build UserManagement Model

--- PTT-NGROUR/Controllers/AdminController.cs
+++ PTT-NGROUR/Controllers/AdminController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace PTT_NGROUR.Controllers
+namespace PTT_NGROUR.App_Start
 {
     public class AdminController : Controller
     {
@@ -14,8 +14,38 @@ namespace PTT_NGROUR.Controllers
 
         public ActionResult UserManagement()
         {
+            var dal = new DAL.DAL();
 
-            return View();
+            var ds = dal.GetDataSet("select * from Users_Auth");
+
+            var dt = ds.Tables[0];
+
+            var listUsers = new List<Models.DataModel.ModelUsersAuth>();
+
+            foreach (System.Data.DataRow dr in dt.Rows)
+            {
+                var user = new Models.DataModel.ModelUsersAuth()
+                {
+                    EMPLOYEE_ID = dr["EMPLOYEE_ID"].ToString(),
+                    FIRSTNAME = dr["FIRSTNAME"].ToString(),
+                    LASTNAME = dr["LASTNAME"].ToString(),
+                    EMAIL = dr["EMAIL"].ToString(),
+                    ROLE_ID = Convert.ToInt32(dr["ROLE_ID"]),
+                    CREATE_DATE = Convert.ToDateTime(dr["CREATE_DATE"]),
+                    CREATE_BY = dr["CREATE_BY"].ToString(),
+                    IS_AD = Convert.ToBoolean(dr["IS_AD"])
+
+                };
+                listUsers.Add(user);
+            }
+
+            var model = new Models.UserManagement()
+            {
+                ModelUsersAuth = listUsers
+            };
+
+            return View(model);
+
         }
 
     }
