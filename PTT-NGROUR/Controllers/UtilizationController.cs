f6000be83bb25b827ee70dffcb8133383fb2039e
Pimthip <pimthip.p@pttdigital.com>
2018-06-24 19:32:28 +0700

    Json for utilization

--- PTT-NGROUR/Controllers/UtilizationController.cs
+++ PTT-NGROUR/Controllers/UtilizationController.cs
@@ -209,7 +209,13 @@ namespace PTT_NGROUR.Controllers
                         NAME = dr["NAME"].ToString(),
                         COLOR = dr["COLOR"].ToString(),
                         VALUE = dr["VALUE"].ToString(),
-                        TYPE = dr["TYPE"].ToString()
+                        TYPE = dr["TYPE"].ToString(),
+                        FLAG = dr["FLAG"].ToString(),
+                        REGION = dr["REGION"].ToString(),
+                        LICENSE = dr["LICENSE"].ToString(),
+                        STATUS = dr["STATUS"].ToString(),
+                        MONTH = dr["MONTH"].ToString(),
+                        YEAR = dr["YEAR"].ToString(),
                     };
                     listRegion.Add(reg);
                 }
--- PTT-NGROUR/Models/DataModel/ModelGetU.cs
+++ PTT-NGROUR/Models/DataModel/ModelGetU.cs
@@ -10,10 +10,14 @@ namespace PTT_NGROUR.Models.DataModel
         public string NO { get; set; }
         public string NAME { get; set; }
         public string VALUE { get; set; }
+        public string FLAG { get; set; } 
         public string COLOR { get; set; }
         public string TYPE { get; set; }
         public string REGION { get; set; }
         public string LICENSE { get; set; }
+        public string STATUS { get; set; }
+        public string MONTH { get; set; }
+        public string YEAR { get; set; } 
 
     }
 
