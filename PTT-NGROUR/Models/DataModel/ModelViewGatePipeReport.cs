494744b8b971d3b6676959649c4adb04324e4eaf
FrankMusix <frankinpark2500@hotmail.com>
2018-07-06 23:09:47 +0700

    Report Serach All Graph

---
+++ PTT-NGROUR/Models/DataModel/ModelViewGatePipeReport.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using PTT_NGROUR.ExtentionAndLib;
+namespace PTT_NGROUR.Models.DataModel
+{
+    public class ModelViewGatePipeReport
+    {
+        public enum GatePipeReportType
+        {
+            NONE , PIPELINE, GATESTATION  
+        }
+
+        public enum GatePipeReportThreshold
+        {
+            NONE , OK, Warning , Alert 
+        }
+
+        public ModelViewGatePipeReport() { }
+
+        public ModelViewGatePipeReport(IDataReader pReader)
+        {
+            if (pReader == null) return;
+            NO = pReader.GetColumnValue("NO").GetInt();
+            NAME = pReader.GetColumnValue("NAME").GetString();
+            REGION = pReader.GetColumnValue("REGION").GetInt();
+            LICENSE_NO = pReader.GetColumnValue("LICENSE_NO").GetInt();
+            VALUE = pReader.GetColumnValue("VALUE").GetDecimal();
+            COLOR = pReader.GetColumnValue("COLOR").GetString();
+            THRESHOLD = pReader.GetColumnValue("THRESHOLD").GetString();
+            MONTH = pReader.GetColumnValue("MONTH").GetInt();
+            YEAR = pReader.GetColumnValue("YEAR").GetInt();
+            TYPE = pReader.GetColumnValue("TYPE").GetString();
+        }
+
+        public int NO { get; set; }
+        public string NAME { get; set; }
+        public int REGION { get; set; }
+        public int LICENSE_NO { get; set; }
+        public decimal VALUE { get; set; }
+        public string COLOR { get; set; }
+        public string THRESHOLD { get; set; }
+        public int MONTH { get; set; }
+        public int YEAR { get; set; }
+        public string TYPE { get; set; }
+
+
+    }
+}
