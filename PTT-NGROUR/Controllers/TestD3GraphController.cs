9fed50906a44fd2f057a602c0460364fcbcfc6f0
porconan <porconan12@hotmail.com>
2018-08-09 14:59:38 +0700

    Update O&M page

---
+++ PTT-NGROUR/Controllers/TestD3GraphController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PTT_NGROUR.Controllers
+{
+    public class TestD3GraphController : Controller
+    {
+        //
+        // GET: /TestD3Graph/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult BarChart()
+        {
+            return View();
+        }
+    }
+}
