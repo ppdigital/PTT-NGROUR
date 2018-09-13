66675cbc6b81ae2aeec4e37e2affef45588098b2
Pimthip <pimthip.p@pttdigital.com>
2018-09-13 09:40:04 +0700

    Production db

--- PTT-NGROUR/DAL/DAL.cs
+++ PTT-NGROUR/DAL/DAL.cs
@@ -9,8 +9,10 @@ namespace PTT_NGROUR.DAL
 {
     public class DAL
     {
-        private string _strConnection = "data source=10.120.2.125:1561/TGIS;password=PTTOUR;persist security info=True;user id=PTTOUR";
-
+        //Production
+        private string _strConnection = "data source=10.120.2.151:1522/PTTGIS2;password=PTTOUR;persist security info=True;user id=PTTOUR";
+        //Test
+        //private string _strConnection = "data source=10.120.2.125:1561/TGIS;password=PTTOUR;persist security info=True;user id=PTTOUR";
         public string ConnectionString { 
             get {
                 return _strConnection;
