3342c80836ba698e62d69cd5cc94fded9fe0a374
Chattarpong Pinsuwan <zchattarpong.p@pttdigital.com>
2019-04-04 16:55:48 +0700

    change data type

--- PTT-NGROUR/Models/DataModel/ModelGetRisk.cs
+++ PTT-NGROUR/Models/DataModel/ModelGetRisk.cs
@@ -22,19 +22,20 @@ namespace PTT_NGROUR.Models.DataModel
             }
             REGION = pReader.GetColumnValue("REGION").GetInt();
             RC_NAME = pReader.GetColumnValue("RC_NAME").GetString();
-            RISK_SCORE = pReader.GetColumnValue("RISK_SCORE").GetInt();
+            LICENSE_NO = pReader.GetColumnValue("LICENSE_NO").GetInt();
+            RISK_SCORE = pReader.GetColumnValue("RISK_SCORE").GetDecimal();
             RISK_SCORE_COLOR = pReader.GetColumnValue("RISK_SCORE_COLOR").GetString();
             RISK_SCORE_RISK = pReader.GetColumnValue("RISK_SCORE_RISK").GetString();
-            INTERNAL_CORROSION = pReader.GetColumnValue("INTERNAL_CORROSION").GetInt();
+            INTERNAL_CORROSION = pReader.GetColumnValue("INTERNAL_CORROSION").GetDecimal();
             INTERNAL_CORROSION_COLOR = pReader.GetColumnValue("INTERNAL_CORROSION_COLOR").GetString();
             INTERNAL_CORROSION_RISK = pReader.GetColumnValue("INTERNAL_CORROSION_RISK").GetString();
-            EXTERNAL_CORROSION = pReader.GetColumnValue("EXTERNAL_CORROSION").GetInt();
+            EXTERNAL_CORROSION = pReader.GetColumnValue("EXTERNAL_CORROSION").GetDecimal();
             EXTERNAL_CORROSION_COLOR = pReader.GetColumnValue("EXTERNAL_CORROSION_COLOR").GetString();
             EXTERNAL_CORROSION_RISK = pReader.GetColumnValue("EXTERNAL_CORROSION_RISK").GetString();
-            THIRD_PARTY_INTERFERENCE = pReader.GetColumnValue("THIRD_PARTY_INTERFERENCE").GetInt();
+            THIRD_PARTY_INTERFERENCE = pReader.GetColumnValue("THIRD_PARTY_INTERFERENCE").GetDecimal();
             THIRD_PARTY_INTERFERENCE_COLOR = pReader.GetColumnValue("THIRD_PARTY_INTERFERENCE_COLOR").GetString();
             THIRD_PARTY_INTERFERENCE_RISK = pReader.GetColumnValue("THIRD_PARTY_INTERFERENCE_RISK").GetString();
-            LOSS_OF_GROUND_SUPPORT = pReader.GetColumnValue("LOSS_OF_GROUND_SUPPORT").GetInt();
+            LOSS_OF_GROUND_SUPPORT = pReader.GetColumnValue("LOSS_OF_GROUND_SUPPORT").GetDecimal();
             LOSS_OF_GROUND_SUPPORT_COLOR = pReader.GetColumnValue("LOSS_OF_GROUND_SUPPORT_COLOR").GetString();
             LOSS_OF_GROUND_SUPPORT_RISK = pReader.GetColumnValue("LOSS_OF_GROUND_SUPPORT_RISK").GetString();
             MONTH = pReader.GetColumnValue("MONTH").GetInt();
@@ -43,19 +44,20 @@ namespace PTT_NGROUR.Models.DataModel
 
         public int REGION { get; set; }
         public string RC_NAME { get; set; }
-        public int RISK_SCORE { get; set; }
+        public int LICENSE_NO { get; set; }
+        public decimal RISK_SCORE { get; set; }
         public string RISK_SCORE_COLOR { get; set; }
         public string RISK_SCORE_RISK { get; set; }
-        public int INTERNAL_CORROSION { get; set; }
+        public decimal INTERNAL_CORROSION { get; set; }
         public string INTERNAL_CORROSION_COLOR { get; set; }
         public string INTERNAL_CORROSION_RISK { get; set; }
-        public int EXTERNAL_CORROSION { get; set; }
+        public decimal EXTERNAL_CORROSION { get; set; }
         public string EXTERNAL_CORROSION_COLOR { get; set; }
         public string EXTERNAL_CORROSION_RISK { get; set; }
-        public int THIRD_PARTY_INTERFERENCE { get; set; }
+        public decimal THIRD_PARTY_INTERFERENCE { get; set; }
         public string THIRD_PARTY_INTERFERENCE_COLOR { get; set; }
         public string THIRD_PARTY_INTERFERENCE_RISK { get; set; }
-        public int LOSS_OF_GROUND_SUPPORT { get; set; }
+        public decimal LOSS_OF_GROUND_SUPPORT { get; set; }
         public string LOSS_OF_GROUND_SUPPORT_COLOR { get; set; }
         public string LOSS_OF_GROUND_SUPPORT_RISK { get; set; }
         public int MONTH { get; set; }
